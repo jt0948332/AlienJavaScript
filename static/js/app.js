b4f49197c864387b40cee97479a6f18ca43657b1
JT <jthorrud@gmail.com>
2019-05-22 11:54:44 -0500

    revrote for reusable function:

--- static/js/app.js
+++ static/js/app.js
@@ -12,29 +12,46 @@ var tablestriped = d3.select("#ufo-table");
 // Need to change the datevalue to value of input field
 //Filter is working as it should
 //I NEED SOMEONE TO EXPLAIN WHAT EXACTLY HAPPENS HERE. WHY 2 FUNCTIONS?
+pageload(tableData);
 
+function pageload(loading){
+  // clear out the table in case there is data there unsing d3 -- not explicitly called
+  tbody.html("")
+  loading.forEach(function (row) {
+    var new_row = tbody.append("tr");
+    new_row.append("td").text(row.datetime);
+    new_row.append("td").text(row.city);
+    new_row.append("td").text(row.state);
+    new_row.append("td").text(row.country);
+    new_row.append("td").text(row.shape);
+    new_row.append("td").text(row.durationMinutes);
+    new_row.append("td").text(row.comments);
+});
+
+}
 
 d3.selectAll("#filter-btn").on("click", function () {
+  d3.event.preventDefault();
     d3.selectAll("td").remove();
     var DateElement = d3.select('#datetime');
     var dateValue = DateElement.property("value");
 function SelectDate(keyDate){
     return keyDate.datetime ===dateValue;
   }
+
   var dateout = data.filter(SelectDate);
   console.log(dateout);
   
-
-      dateout.forEach(function (row) {
-        var new_row = tbody.append("tr");
-        new_row.append("td").text(row.datetime);
-        new_row.append("td").text(row.city);
-        new_row.append("td").text(row.state);
-        new_row.append("td").text(row.country);
-        new_row.append("td").text(row.shape);
-        new_row.append("td").text(row.durationMinutes);
-        new_row.append("td").text(row.comments);
-    });
-    d3.event.preventDefault();
+  pageload(dateout);
+    //   dateout.forEach(function (row) {
+    //     var new_row = tbody.append("tr");
+    //     new_row.append("td").text(row.datetime);
+    //     new_row.append("td").text(row.city);
+    //     new_row.append("td").text(row.state);
+    //     new_row.append("td").text(row.country);
+    //     new_row.append("td").text(row.shape);
+    //     new_row.append("td").text(row.durationMinutes);
+    //     new_row.append("td").text(row.comments);
+    // });
     
 });
